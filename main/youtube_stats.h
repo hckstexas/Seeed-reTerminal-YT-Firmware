fe6278a50d5879e56b68ebfbaf85321148f75a88
hckstexas <158211598+hckstexas@users.noreply.github.com>
2026-09-03 10:25:53 -0500

    feat: use saved Wi-Fi and endpoint configuration

--- main/youtube_stats.h
+++ main/youtube_stats.h
@@ -2,6 +2,8 @@
 
     #include <cstdint>
 
+struct YoutubeConfig;
+
     struct YoutubeStats {
       bool valid = false;
       uint64_t subscribers = 0;
@@ -10,5 +12,5 @@
       char channel_title[96] = {};
     };
 
-    bool fetch_youtube_stats(YoutubeStats &stats, char *error_message, uint32_t error_message_size);
+    bool fetch_youtube_stats(const YoutubeConfig &config, YoutubeStats &stats, char *error_message, uint32_t error_message_size);
     
